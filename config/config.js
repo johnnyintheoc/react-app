072ea56b57ed705722f4172b5ccc0963183940f3
Johnny <johnnyoc@gmail.com>
2019-11-19 23:34:29 -0800

    updated package file

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const keys =  {
+    host: "remotemysql.com",
+    database: "gnf0NqfHSB",
+    user: "gnf0NqfHSB",
+    password: "6vh3vr6yX4",
+    port: 3306,
+    YoutubeApiKey: "AIzaSyDiIgARVnc721NbTztucD0SuQyIuBXL4VA"
+};
+
+module.exports = keys;
