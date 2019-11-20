e12a3ecee921f684d038ec26099d6e0cc3326c49
Johnny <johnnyoc@gmail.com>
2019-11-19 22:19:21 -0800

    comment updates

--- server/server.js
+++ server/server.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-/*
-let connection = mysql.createConnection({
-    host: "remotemysql.com",
-    user: "gnf0NqfHSB",
-    password: "6vh3vr6yX4", // YOUR_DB_PASSWORD update with your database password
-    database: "gnf0NqfHSB", // YOUR_DB update this to reflect your database
-});
-*/
-
 // Routes
 const routeHandler = require('../routes/handler.js');
 
@@ -23,10 +14,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}.`);
 });
-
-/*
-<Route path="/tech" exact component={Tech} />
-            <Route path="/tesla/:id" component={TeslaDetails} />
-            <Route path="/tech/:id" component={TechDetails} />
-            <Route path="/news/:id" component={NewsDetails} />
-            */
