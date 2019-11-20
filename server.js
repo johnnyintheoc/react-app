1c7c141f8b5a0b686afbb9d32ffe74a22cbac6d4
Johnny <johnnyoc@gmail.com>
2019-11-20 14:24:28 -0800

    express server update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const path = require('path');
+const path = require('path');
 
 // Routes
 const routeHandler = require('./routes/handler.js');
@@ -11,20 +11,21 @@ app.use(bodyParser.json());
 app.use('/', routeHandler);
 //app.use(express.static(path.join(__dirname,'client/build')), routeHandler);
 
-/*
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    //app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, 'client/build', routeHandler));
     });
 }
-*/
 
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     //console.log(`Listening on port ${PORT}.`);
 });
+
+
+// https://github.com/esausilva/react-production-deployment/tree/master/heroku
