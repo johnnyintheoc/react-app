e672a878ac2551e023aaec3a876fdc893b6efc7a
Johnny <johnnyoc@gmail.com>
2019-11-20 00:44:01 -0800

    package update

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use('/', routeHandler);
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}.`);
+    //console.log(`Listening on port ${PORT}.`);
 });
