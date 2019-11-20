2b69d5767f0b31a1d55c10cbe2a11ec592fa5f4c
Johnny <johnnyoc@gmail.com>
2019-11-20 02:26:23 -0800

    package dependencies

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+//const path = require('path');
 
 // Routes
 const routeHandler = require('./routes/handler.js');
@@ -8,6 +9,19 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routeHandler);
+//app.use(express.static(path.join(__dirname,'client/build')), routeHandler);
+
+/*
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    //app.use(express.static(path.join(__dirname, 'client/build')));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', routeHandler));
+    });
+}
+*/
 
 // Start Server
 const PORT = process.env.PORT || 5000;
